073a1b76deddeb7d1aacca1a7ed50ec7b80ad111
Al Graven <alex_graven242@yahoo.com>
2016-07-12 22:35:43 -0400

    Adds winner method to Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
-  
+
   [:player_1_score, :player_2_score].each do |attribute|
 
     validates attribute, presence: true
@@ -7,7 +7,10 @@ class Game < ActiveRecord::Base
 
   end
 
-    belongs_to :player_1, class_name: Player, foreign_key: 'player_1_id', required: true
-    belongs_to :player_2, class_name: Player, foreign_key: 'player_2_id', required: true
+    belongs_to :player_1, class_name: 'Player', foreign_key: 'player_1_id', required: true
+    belongs_to :player_2, class_name: 'Player', foreign_key: 'player_2_id', required: true
 
+    def winner
+      (player_1_score > player_2_score) ? player_1 : player_2
+    end
 end
