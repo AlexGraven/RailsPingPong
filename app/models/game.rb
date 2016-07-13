ff0a1a0201dcaada38df342aa5eab5e18c0160d2
Al Graven <alex_graven242@yahoo.com>
2016-07-12 22:03:46 -0400

    Does the Rspec RIGHT

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,13 @@
 class Game < ActiveRecord::Base
-  [:player_1_id, :player_2_id,
-   :player_1_score, :player_2_score].each do |attribute|
+  
+  [:player_1_score, :player_2_score].each do |attribute|
 
     validates attribute, presence: true
     validates attribute, numericality: {only_integer: true}
+
   end
+
+    belongs_to :player_1, class_name: Player, foreign_key: 'player_1_id', required: true
+    belongs_to :player_2, class_name: Player, foreign_key: 'player_2_id', required: true
+
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
 
-  it {should validate_presence_of(:player_1_id)}
-  it {should validate_numericality_of(:player_1_id)}
-  it {should validate_presence_of(:player_2_id)}
-  it {should validate_numericality_of(:player_2_id)}
+  it {should belong_to(:player_1)}
+  it {should belong_to(:player_2)}
+
   it {should validate_presence_of(:player_1_score)}
   it {should validate_numericality_of(:player_1_score)}
   it {should validate_presence_of(:player_2_score)}
