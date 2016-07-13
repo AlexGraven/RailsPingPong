6dcc645252dbeeb43ec166502fe1da14fc38c77b
Al Graven <alex_graven242@yahoo.com>
2016-07-13 16:33:05 -0400

    Adds HTML5 validations and order by grade

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,10 +1,12 @@
 class Player < ActiveRecord::Base
 
+  VALID_HANDS = ["Right", "Left"]
+
   [:name, :email, :hand, :grade].each do |attribute|
     validates attribute, presence: true
   end
 
-  validates :hand, inclusion: { in: %w(Right Left),
+  validates :hand, inclusion: { in: VALID_HANDS,
     message: "%{value} is not a valid hand" }
   validates :grade, numericality: { only_integer: true }
 
