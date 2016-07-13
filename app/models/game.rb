e2e0c912b277f5817af1461e31e47066dec60c31
Al Graven <alex_graven242@yahoo.com>
2016-07-12 21:14:54 -0400

    Makes Rspec for Game pass

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,8 @@
 class Game < ActiveRecord::Base
+  [:player_1_id, :player_2_id,
+   :player_1_score, :player_2_score].each do |attribute|
+
+    validates attribute, presence: true
+    validates attribute, numericality: {only_integer: true}
+  end
 end
